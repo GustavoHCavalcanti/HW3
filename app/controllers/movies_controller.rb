abae837b437f1126e7f1a51e065364bc040e170d
Gustavo <gustavo.h.a.cavalcanti@gmail.com>
2025-09-16 22:01:12 -0300

    código pronto

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,32 @@
 # This file is app/controllers/movies_controller.rb
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+  @all_ratings = Movie.all_ratings
+
+  # ler parâmetros/lembrar sessão
+  @sort = params[:sort] || session[:sort]
+  @checked_ratings = (params[:ratings]&.keys || session[:ratings] || @all_ratings)
+
+  # manter URL canônica (para os testes): sort + ratings
+  if params[:sort] != @sort || (params[:ratings]&.keys || session[:ratings]) != @checked_ratings
+    flash.keep
+    return redirect_to movies_path(sort: @sort, ratings: Hash[@checked_ratings.map { |r| [r, '1'] }])
+  end
+
+  # consulta: filtra e ordena
+  @movies = Movie.where(rating: @checked_ratings)
+  case @sort
+  when 'title'
+    @movies = @movies.order(:title)
+    @title_hilite = 'hilite'
+  when 'release_date'
+    @movies = @movies.order(:release_date)
+    @date_hilite = 'hilite'
+  end
+
+  # salvar preferências
+  session[:sort]    = @sort
+  session[:ratings] = @checked_ratings
   end
 
   def show
